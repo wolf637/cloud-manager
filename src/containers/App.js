81ba0a56380d18525beb512f2d4bcca8afce5ee3
wolf637 <alexey.volkov@live.com>
2018-10-09 21:54:47 -0700

    Create redux store; setup App container

---
+++ src/containers/App.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class App extends Component{
+  render(){
+    
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,26 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import NavBar from './components/NavBar';
-import VMList from './components/VMList';
-import Jumbo from './components/Jumbo';
-import EnvList from './components/env_list';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import App from './containers/App';
+import CloudManagerApp from './reducers';
+import thunkMidleware from 'redux-thunk';
 
-class VMManager extends React.Component {
+
+
+// import './index.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import NavBar from './components/NavBar';
+// import VMList from './components/VMList';
+// import Jumbo from './components/Jumbo';
+// import EnvList from './components/env_list';
+
+
+
+
+
+let createStoreWithMiddleware = applyMiddleware(thunkMidleware)(createStore)
+
+let store = createStoreWithMiddleware(CloudManagerApp)
+
+class CloudManager extends React.Component {
 
   render() {
     return (
-      <div>
-        <NavBar />
-        <Jumbo />
-        <EnvList />
-      </div>
+      <Provider store={store}>
+        <App />
+      </Provider>
       )
   }
 }
 
 ReactDOM.render(
-  <VMManager />,
+  <CloudManager />,
   document.getElementById('root')
 );
